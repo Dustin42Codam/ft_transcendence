28466bfd437a45c1bca01003c9687a2d41254c5c
Ryno95 <rmeiboom@student.codam.nl>
2022-09-14 15:43:19 +0200

    added script for executing tests

--- runTests.sh
+++ runTests.sh
@@ -1,11 +1,17 @@
-docker-compose up --build &
+# start up the application as specific in subject
+docker-compose up --build &>/dev/null &
 
 
+# give server time to start-up
+sleep 15
 
 
-# EXI_CODE = executeTests.sh
+# EXIT_CODE = executeTests.sh
 EXIT_CODE=0
 
+# Kill all containers to stop the CI process
 docker kill $(docker ps -q)
 
+
+# Ecit with exit code from tests ran
 exit $EXIT_CODE
