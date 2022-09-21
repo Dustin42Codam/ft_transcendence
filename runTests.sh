56332f8bf1aedb2b391bc4121d510322348e42f2
krusts31 <alkrusts@student.codam.nl>
2022-09-21 20:39:05 +0200

    added health checks for circleci

--- runTests.sh
+++ runTests.sh
@@ -1,17 +1,28 @@
 # start up the application as specific in subject
-docker-compose up --build &>/dev/null &
+# here we use -d to put the docker containers in to the bg for testing purposes
+docker-compose up --build -d
 
-
-# give server time to start-up
-sleep 25
-
-
-# EXIT_CODE = executeTests.sh
-EXIT_CODE=0
+need_healthy_services=2
+healthy_service_count=0
+while [ true ]
+do
+	docker-compose ps
+	healthy_service_count=$(docker-compose ps | grep "healthy" | wc -l)
+	if [ $(docker-compose ps | grep "unhealthy" | wc -l) -gt 0 ]
+	then
+		docker-compose ps
+		docker-compose down
+		exit 1
+	fi
+	if [ $need_healthy_services -eq $healthy_service_count ]
+	then
+		break
+	fi
+	sleep 10
+done
 
 # Kill all containers to stop the CI process
-docker kill $(docker ps -q)
-
+docker-compose down
 
 # Ecit with exit code from tests ran
 exit $EXIT_CODE
