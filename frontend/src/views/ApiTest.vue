c2d4ff74a57402c85430f7af7c645f9ed1cc9411
krusts31 <alkrusts@student.codam.nl>
2022-09-24 22:37:17 +0200

    added a simple api for testin vies: api does a post request ot localhost:3000

---
+++ frontend/src/views/ApiTest.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+
+import axios from 'axios';
+import { ref } from "vue";
+
+const email: string = ref("");
+const password: string = ref("");
+
+function onSubmit() {
+  console.log(email.value);
+  console.log(password.value);
+  axios({
+    method: "post",
+    url: "http://localhost:3000/",
+    data:{
+      username: email.value,
+      password: password.value
+    }
+  });
+}
+
+</script>
+
+<template>
+  <div>
+    <b-form @submit="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label="Intra Email address:"
+        label-for="input-1"
+        description="We'll use 42.api for auth"
+      >
+        <b-form-input
+          id="input-1"
+          type="email"
+          placeholder="Enter email"
+          v-model="email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Intra Password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="password"
+          placeholder="Enter password"
+          v-model="password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
