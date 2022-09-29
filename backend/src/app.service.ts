40b15d140939a6483750eb19d27ff186fc459728
Ryno95 <rmeiboom@student.codam.nl>
2022-09-29 15:22:28 +0200

    added simple post method to check incomng data from frontend

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello  World!';
+    return 'Hello World!';
+  }
+  
+  PostFromVue(): string {
+    return 'Hello Vue!';
   }
 }
