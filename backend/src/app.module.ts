04da4b5343c01414b4efb0f773b26137d5456398
lizbisschop <lizbisschop@hotmail.com>
2022-09-30 18:05:44 +0200

    meeh stupid stuff

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import {ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+
+
+console.log(process.env.POSTGRES_URL);
+console.log(process.env.POSTGRES_USER);
+console.log(process.env.POSTGRES_PASSWORD);
+console.log(process.env.POSTGRES_DB);
 @Module({
-  imports: [],
+  imports: 
+  [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync
+    ({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: 'postgres',
+          url: configService.get('DATABASE_URL'), 
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
+          entities: [],
+          autoLoadEntities: true,
+          synchronize: true
+        }),
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
