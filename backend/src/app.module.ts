504955989de7be4b9012de75b718a81f56190044
Ryno95 <rmeiboom@student.codam.nl>
2022-10-04 13:14:06 +0200

    added chat table to db. Connected basic CRUD entry points

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import {ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'dotenv';
+import { ChatModule } from './chat/chat.module';
 
 const pathToEnv: string = '.env';
 config({path: pathToEnv});
@@ -24,7 +25,8 @@ config({path: pathToEnv});
           synchronize: true
         }),
     }),
-    UsersModule
+    UsersModule,
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/chat/entities/chat.entity.ts
+++ backend/src/chat/entities/chat.entity.ts
@@ -1,1 +1,40 @@
-export class Chat {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+
+@Entity()
+class Chat {
+    @PrimaryColumn()
+    public id: number;
+
+    @Column()
+    public name: string;
+
+    // add protection for olny 3 possible choices
+    @Column()
+    public visibility: string;
+
+    @Column("simple-array")
+    public admin: string[];
+
+    @Column("simple-array")
+    public connectedUsers: string[];
+
+    @Column("simple-array")
+    public mutedUsers: string[];
+
+    @Column("simple-array")
+    public banendUsers: string[];
+
+    @Column()
+    public password: string; // hashed and salted
+}
+
+export default Chat;
+// id: number
+// name: string
+// visibility: string (public, private, protect)
+// admin: string[usernames]
+// connecter_users: string[usernames]
+// password: string (salted & hashed)
+// muted_users: string[usernames]
+// banend_users: string[usernames]
