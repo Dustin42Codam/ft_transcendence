2d1fb21b7aa58310586999c56dd99a284e5acdb1
liz bisschop <lizbisschop@hotmail.com>
2022-12-15 14:33:57 +0100

    using cookie

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -7,21 +7,21 @@ export class AuthGuard implements CanActivate {
 	constructor(private jwtService: JwtService) {}
 	canActivate(context: ExecutionContext)
 	{
-		// const request = context.switchToHttp().getRequest();
-		// console.log("trying to printout the session");
-		// console.log(request.session);
-		// try {
-		// 	if (request.session.logged_in != true)
-		// 		return false;
-		// 	const jwt = request.cookies['jwt'];
-		// 	return this.jwtService.verify(jwt);
-		// } catch (e) {
-		// 	return false;
-		// }
-		console.log("checking the authguard");
 		const request = context.switchToHttp().getRequest();
-		if (request.session.logged_in != true)
-			throw new BadRequestException("You are not logged in");
-		return request.session.logged_in;
+		console.log("trying to printout the session");
+		console.log(request.session);
+		try {
+			if (request.session.logged_in != true)
+				return false;
+			const jwt = request.cookies['jwt'];
+			return this.jwtService.verify(jwt);
+		} catch (e) {
+			return false;
+		}
+		// console.log("checking the authguard");
+		// const request = context.switchToHttp().getRequest();
+		// if (request.session.logged_in != true)
+		// 	throw new BadRequestException("You are not logged in");
+		// return request.session.logged_in;
 	}
 }
