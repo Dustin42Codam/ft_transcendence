ed60efccfa4a076ed4d3618912919091ebc8e854
Aleksandrs Marks Krusts <alkrusts@pop-os.localdomain>
2022-12-15 16:42:06 +0200

    meess of a gell

--- backend/src/web-socket/web-socket.gateway.ts
+++ backend/src/web-socket/web-socket.gateway.ts
@@ -1,16 +1,26 @@
 import { WebSocketServer, OnGatewayConnection, WsResponse, OnGatewayInit, MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { MemberService } from "src/member/member.service";
+import { UserService } from "src/user/user.service";
+import { Member } from "src/member/entity/member.entity";
 import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
+import { AuthGuard } from '../auth/auth.guard';
+
+//user needs cookie
+//user needs to be authorized 
+//if I join a chat I get a cookie for that chat?
+//that I can make sure that I can send message to that chat
 
 @WebSocketGateway({
   cors: { credentials: true, methods: ['GET', 'POST'], origin: ['http://localhost:4242', 'http://localhost:3000']},
   transports: ['polling', 'websocket']})
 export class WebSocketGateways implements OnGatewayInit, OnGatewayConnection {
+	constructor(private readonly userService: UserService,
+    private readonly memberService: MemberService) {};
 
 	private logger: Logger = new Logger("AppGateway");
 
 	afterInit(server: Server) {
-	//You know that green stuff when api boots up? It will print something at start up
 		this.logger.log("socket.io websocket server is inited!");
 	}
 
@@ -23,16 +33,21 @@ export class WebSocketGateways implements OnGatewayInit, OnGatewayConnection {
   }
 	//IF IS A MEMBER OF A CHAT THIS ROOM before connection and it AUTH
 	//if payload is undefined it becomes null
+
+	//@UseGuards(AuthGuard)
   @SubscribeMessage('msgToServer')
   handleMessage(client: Socket, payload: string): WsResponse<string> {
-		//IF I WANT TO SEND
-		//I AM NOT RESITRICTED->muted and blocked
-		//IF I WANT TO READ
-		//you can allways read
-		//message should be filtered
-		console.log(payload);
-		//returns data to the client
-		return { event: "msgToClient", data: payload};
+
+		/*
+    const member: Member = await this.memberService.getMemberById(Number(body.member));
+    if (this.memberService.isRestricted(member)) {
+      throw new BadRequestException("You are restricted from this chatroom.");
+			return { event: "msgNotRecivedToClient", data: null};
+    }
+    this.messageService.create({timestamp: new Date(), member: member, message: body.message});
+	 */
+		console.log("payload", payload);
+		return { event: "msgRecivedToClient", data: null};
   }
 	/*
 	@WebSocketServer ws: Server;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,42 +3,93 @@ import { io, Socket } from "socket.io-client";
 import Wrapper from "../components/Wrapper";
 import { fetchChats } from "../redux/chat/chatActions";
 
+/*
 const Dashboard = () => {
-  const socket: Socket = io("ws://localhost:3000", {
-    withCredentials: true,
-    transports: ["websocket", "polling"],
-  });
+	const client = useRef<Socket | null>(null);
   const [newMsg, setNewMsg] = useState<string>("");
-  const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
 
   useEffect(() => {
+		if (client.current == null) {
+		}
+			/*
     const connectToServer = async () => {
-      console.log("conneing");
       await new Promise<void>((resolve) => {
-        setIsConnected(true);
-        console.log("connected again");
-        socket.on("connect", () => resolve());
+        client.on("connect", () => {
+          setIsConnected(true), resolve();
+        });
       });
     };
-    if (isConnected == false) {
-      connectToServer();
-    }
+			client.current.on('connect', () => console.log(`Client connected: ${client.current!.id}`));
+
+			client.current.on('disconnect', (reason) =>
+				console.log(`Client disconnected: ${reason}`)
+			);
+
+			client.current.on('connect_error', (reason) =>
+				console.log(`Client connect_error: ${reason}`)
+			);
+
+			function reciveMessage(msg: string) {
+				client.current!.on('msgRecivedToClient', (data) => console.log("DATA", data));
+			}
+
   });
-  console.log("done connecting", isConnected);
+	/*
+	useEffect( () => {
+		return  () => {
+			console.log("unmount");
+			client.current!.close();
+		}
+	}
+	, []);
+
 
   function sendMessage(msg: string) {
-    socket.emit("msgToServer", msg);
-  }
-  function reciveMessage(msg: string) {}
+    console.log(msg);
+    client.current!.emit("msgToServer", msg);
+	}
   //const ws = new WebSocket("ws://localhost:3000");
   return (
     <Wrapper>
       <input type="msg" onChange={(e) => setNewMsg(e.target.value)}></input>
       <button onClick={() => sendMessage(newMsg)}>Submit</button>
       <div>Dashboard</div>
-      <p>Connected: {"" + isConnected}</p>
     </Wrapper>
   );
 };
+*/
+const Dashboard = () => {
+  const socketRef = useRef<Socket | null>(null);
+  const [settings, setSettings] = useState(false);
 
+  useEffect(() => {
+    if (socketRef.current == null) {
+      socketRef.current = io('ws://localhost:3000', {
+					withCredentials: true,
+					transports: ["websocket", "polling"],
+			});
+		}
+
+    const {current: socket} = socketRef;
+
+    try {
+      socket.open();
+      socket.emit("msgToServer", 'load settings');
+      socket.on('msgRecivedToClient', (data) => {
+        setSettings(data);
+      })
+    } catch (error) {
+      console.log(error);
+    }
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <Wrapper>
+      <p>{settings}</p>
+    </Wrapper>
+	);
+};
 export default Dashboard;
