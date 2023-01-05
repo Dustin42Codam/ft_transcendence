a231301c328e130820fce6c3af731d1e48fe4b92
Aleksandrs Marks Krusts <alkrusts@pop-os.localdomain>
2023-01-05 16:04:46 +0200

    listening to button preses for the game

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -4,13 +4,26 @@ import loadGame from "../modules/Game";
 import "./Game.css";
 
 const Game = (props: any) => {
+
+	function myKeyPress(e: any){
+		let keynum: any;
+
+		if (window.event) { // IE
+			keynum = e.keyCode;
+		} else if (e.which){
+			keynum = e.which;
+		}
+
+		console.log(String.fromCharCode(keynum));
+	}
   useEffect(() => {
     loadGame();
   }, []);
 
   return (
     <Wrapper>
-        <canvas id="canvas" width="1300" height="700"></canvas>
+        <canvas onKeyDown={(e) => myKeyPress(e)} tabIndex={0} id="canvas" width="1300" height="700">
+				</canvas>
     </Wrapper>
   );
 };
