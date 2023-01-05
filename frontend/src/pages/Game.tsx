398de8ddb7497ea2795f2fde0e699203ea3f356e
Aleksandrs Marks Krusts <alkrusts@pop-os.localdomain>
2023-01-05 14:53:57 +0200

    added the game

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,12 +1,21 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Wrapper from "../components/Wrapper";
+import loadGame from "../modules/Game";
 
-export default class Game extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <div>Games</div>
-      </Wrapper>
-    );
-  }
-}
+const Game = (props: any) => {
+
+	useEffect(() => {
+		loadGame();
+	}, []);
+
+  return (
+    <Wrapper>
+      <div>
+				<canvas id="canvas" width="500" height="300">
+				
+				</canvas>
+			</div>
+    </Wrapper>
+  );
+};
+export default Game;
