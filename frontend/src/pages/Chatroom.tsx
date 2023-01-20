47702048c624c3b872018d4aa6b4e5fe489111fb
Dustin42Codam <dkrecisz@student.codam.nl>
2023-01-20 14:34:40 +0100

    Add: Chatroom component

---
+++ frontend/src/pages/Chatroom.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wrapper from '../components/Wrapper'
+
+function Chatroom() {
+  return (
+	<Wrapper>
+		<div>Chatroom</div>
+	</Wrapper>
+  )
+}
+
+export default Chatroom
