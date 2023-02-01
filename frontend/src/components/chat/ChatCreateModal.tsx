853c81961e0b2b504f1f9281167d5c06d4bb7ed1
Dustin42Codam <dkrecisz@student.codam.nl>
2023-02-01 22:47:08 +0100

    Add new create chat pop up

---
+++ frontend/src/components/chat/ChatCreateModal.tsx
@@ -0,0 +1,156 @@
+import AddIcon from '@mui/icons-material/Add';
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Col, FloatingLabel } from "react-bootstrap";
+import { ChatroomType } from "../../models/Chats";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { selectCurrentUser } from "../../redux/slices/currentUserSlice";
+import { addNewGroupChat, fetchGroupChats } from "../../redux/slices/chatsSlice";
+import { toast } from "react-toastify";
+import { useState } from 'react';
+
+function ChatCreateModal() {
+	const [show, setShow] = useState(false);
+	const [chatName, setChatName] = useState("");
+	const [chatType, setChatType] = useState(ChatroomType.PUBLIC);
+	const [password, setPassword] = useState("");
+	const [passwordConfirm, setPasswordConfirm] = useState("");
+	const currentUser = useAppSelector(selectCurrentUser);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	const dispatch = useAppDispatch();
+
+	async function createChat() {
+		if (!chatName) {
+		  toast.error("Name cannot be empty!", {
+			position: "top-center",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "colored",
+		  });
+		} else if (
+		  chatType === ChatroomType.PROTECTED &&
+		  password !== passwordConfirm
+		) {
+		  toast.error("Passwords did not match!", {
+			position: "top-center",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "colored",
+		  });
+		} else {
+		  dispatch(
+			await addNewGroupChat({
+			  chat: {
+				name: chatName,
+				password: password,
+				user_ids: [],
+				type: chatType,
+			  },
+			  user_id: currentUser.id,
+			})
+		  );
+		  dispatch(fetchGroupChats());
+		}
+	  }
+
+  return (
+	<div>
+		<div  onClick={handleShow}>
+			<AddIcon/>
+			Create channel
+		</div>
+		<Modal show={show} onHide={handleClose}>
+        	<Modal.Header closeButton>
+        	  <Modal.Title>Create a Channel</Modal.Title>
+        	</Modal.Header>
+
+			<Modal.Body>
+				<Form>
+					{/* +++++ Channel Name +++++ */}
+					<Form.Group>
+						<Form.Label>
+							Name
+						</Form.Label>
+						<Form.Control
+							type='name'
+							autoFocus
+							onChange={(e: any) => setChatName(e.target.value)}
+						/>
+					</Form.Group>
+					{/* ++++++++++++++++++++++++ */}
+
+					{/* +++++ Channel Type +++++ */}
+					<Col md>
+						<FloatingLabel
+						controlId="floatingSelectGrid"
+						label="Channel Type"
+						className="mb-3"
+						>
+							<Form.Select
+								aria-label="Select Type"
+								defaultValue={ChatroomType.PUBLIC}
+								onChange={(e: any) => setChatType(e.target.value)}
+							>
+								<option value={ChatroomType.PRIVATE}>
+								{ChatroomType.PRIVATE}
+								</option>
+								<option value={ChatroomType.PROTECTED}>
+								{ChatroomType.PROTECTED}
+								</option>
+								<option value={ChatroomType.PUBLIC}>
+								{ChatroomType.PUBLIC}
+								</option>
+							</Form.Select>
+						</FloatingLabel>
+					</Col>
+					{/* ++++++++++++++++++++++++ */}
+
+					{/* +++++ Channel Password +++++ */}
+            		<Form.Floating className="mb-3">
+            		  <Form.Control
+            		    id="floatingInputCustom"
+            		    type="password"
+            		    placeholder="Password"
+            		    onChange={(e: any) => setPassword(e.target.value)}
+            		    disabled={chatType !== ChatroomType.PROTECTED}
+						/>
+            		  <label htmlFor="floatingInputCustom">Password</label>
+            		</Form.Floating>
+					{/* +++++ Password Confirm +++++ */}
+            		<Form.Floating>
+            		  <Form.Control
+            		    id="floatingPasswordConfirmCustom"
+            		    type="password"
+            		    placeholder="Password Confirm"
+            		    onChange={(e: any) => setPasswordConfirm(e.target.value)}
+            		    disabled={chatType !== ChatroomType.PROTECTED}
+            		  />
+            		  <label htmlFor="floatingPasswordConfirmCustom">
+            		    Password Confirm
+            		  </label>
+            		</Form.Floating>
+            		{/* ++++++++++++++++++++++++++++ */}
+				</Form>
+			</Modal.Body>
+
+			<Modal.Footer>
+          		<Button variant="primary" onClick={createChat}>
+          		  Save
+          		</Button>
+			</Modal.Footer>
+		</Modal>
+	</div>
+  )
+}
+
+export default ChatCreateModal
